d3fae119db654603e77d36d137c8eaef9adbd1ad
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-24 00:54:46 +0600

    44-7 Axios, Data Extraction And Data Processing Bar Chart complete

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -4,6 +4,20 @@ import axios from "axios";
 import { data } from "autoprefixer";
 //https://axios-http.com/docs/intro
 //https://github.com/ProgrammingHero1/broken-phone?tab=readme-ov-file
+//https://recharts.org/en-US/examples/BarChartWithCustomizedEvent
+//44-7 Axios, Data Extraction And Data Processing Bar Chart
+
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
@@ -23,18 +37,40 @@ const Phones = () => {
       .get(`https://openapi.programming-hero.com/api/phones?search=iphone`)
       //   .then((data) => console.log(data));
       //   .then((data) => console.log(data.data.data));
-      .then((data) => setPhones(data.data.data));
+      //   .then((data) => setPhones(data.data.data));
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phnDetailsData = phoneData.map((phn) => {
+          const obj = {
+            name: phn.phone_name,
+            price: parseInt(phn.slug.split("-")[1]),
+          };
+          return obj;
+        });
+        console.log(phnDetailsData);
+        setPhones(phnDetailsData);
+      });
   }, []);
+  console.log(phones);
 
   return (
     <div>
       <h1>Phone:{phones.length}</h1>
-      {phones.map(() => (
+      {/* {phones.map(() => (
         <Phone></Phone>
-      ))}
+      ))} */}
+      <BarChart width={1000} height={600} data={phones}>
+        <XAxis dataKey="name"></XAxis>
+        <YAxis dataKey="price"></YAxis>
+        <Tooltip></Tooltip>
+        <Bar dataKey="price" fill="green">
+          {/* {phones.map((entry, index) => (
+            <Cell cursor="pointer" fill={"#82ca9d"} key={`cell-${index}`} />
+          ))} */}
+        </Bar>
+      </BarChart>
     </div>
   );
 };
 
 export default Phones;
-//7 min
